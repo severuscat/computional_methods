fda5ebbfce5b72dd2dd8f7d9f2ecaa954f951bd8
severuscat <yan_os@mail.ru>
2020-05-10 19:32:11 +0300

    tests fixed

--- test.py
+++ test.py
@@ -4,21 +4,25 @@ import algorithm
 def check(result, expected, precision):
     if result.size != expected.size: return False
     for i in range(result.size):
-        if abs(expected.get(i) - result.get(i)) > precision:
+        if abs(expected.vector[i] - result.vector[i]) > precision:
             return False
+    return True
 
 
 def small_test():
     matrix = algorithm.Matrix(
         matrix=[[1, 1],
                 [1, -2]],
-        result=[0, 1],
+        result=algorithm.Vector(
+            vector=[0, 1],
+            size=2
+        ),
         size=2
     )
     precision = 0.00001
     result = algorithm.run_algorithm(matrix, precision)
     expected = algorithm.Vector(
-        vector=[0.3333, -0.3333],
+        vector=[0.33333, -0.33333],
         size=2
     )
     assert (check(result, expected, precision))
